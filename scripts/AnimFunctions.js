e11215a4746f395dba83c07ad5844e8d8bfdf31b
Faizan Ahmad <cfaizana@gmail.com>
2021-09-04 12:33:22 -0400

    Detect magic only visible for caster and GM

--- scripts/AnimFunctions.js
+++ scripts/AnimFunctions.js
@@ -1,13 +1,19 @@
 Hooks.once('init', async function () {
-    async function detectMagic(rollData){
+
+    //Effect functions to be called from a macro in the "OnUseMacro" field of MIDI-QOL
+    //Each function is excuted via socketlib to ensure proper permissions for the effect
+    async function detectMagic(rollData) {
         socket.executeAsGM("detectMagic", rollData);
     }
-    
+
+
+    // List of effects that can be called
     game.AdvancedSpellEffects = {};
     game.AdvancedSpellEffects.detectMagic = detectMagic;
 });
 
 Hooks.once("socketlib.ready", () => {
+    //Define every function needed for an effect here so that they can be registered by socketlib
     async function detectMagicContinous(rollData) {
         // Wasp - Sequencer guy Detect Magic Macro
         // Requires the JB2A patreon module
@@ -22,7 +28,8 @@ Hooks.once("socketlib.ready", () => {
         Hooks.off("updateToken", detectMagicHookId);
         await TokenMagic.deleteFilters(tokenD, "detectMagicGlow");
         */
-       let args = rollData;
+        let args = rollData;
+        //console.log("ARGS: ", args);
         let error = false;
         if (typeof args !== 'undefined' && args.length === 0) {
             error = `You can't run this macro from the hotbar! This is a callback macro. To use this, enable MidiQOL settings in "Workflow" -> "Add macro to call on use", then add this macro's name to the bottom of the Misty Step spell in the "On Use Macro" field.`;
@@ -42,7 +49,32 @@ Hooks.once("socketlib.ready", () => {
             error = `You need to have MidiQOL ${installed} to run this macro!`;
         }
 
+        if (!game.modules.get("tokenmagic")?.active) {
+            let installed = game.modules.get("tokenmagic") && !game.modules.get("tokenmagic").active ? "enabled" : "installed";
+            error = `You need to have Token Magic FX ${installed} to run this macro!`;
+        }
+
+        if (!game.modules.get("socketlib")?.active) {
+            let installed = game.modules.get("socketlib") && !game.modules.get("socketlib").active ? "enabled" : "installed";
+            error = `You need to have SocketLib ${installed} to run this macro!`;
+        }
+
+        if (!game.modules.get("tagger")?.active) {
+            let installed = game.modules.get("tagger") && !game.modules.get("tagger").active ? "enabled" : "installed";
+            error = `You need to have TTagger${installed} to run this macro!`;
+        }
+
+        if (!game.modules.get("sequencer")?.active) {
+            let installed = game.modules.get("sequencer") && !game.modules.get("sequencer").active ? "enabled" : "installed";
+            error = `You need to have Sequencer ${installed} to run this macro!`;
+        }
         const actorD = game.actors.get(args[0].actor._id);
+        let users = [];
+        for (const user in actorD.data.permission){
+            if (user == "default") continue;
+            users.push(user);
+        }
+        console.log("Visible to users: " , users);
         const tokenD = canvas.tokens.get(args[0].tokenId);
         const itemD = actorD.items.getName(args[0].item.name);
 
@@ -79,7 +111,7 @@ Hooks.once("socketlib.ready", () => {
             .belowTiles()
             .scale(2.33333)
             .thenDo(async () => {
-                await registerDetectMagicHook(tokenD);
+                await registerDetectMagicHook(tokenD, users);
             })
             .thenDo(async () => {
                 await applyMagicHighlight();
@@ -93,6 +125,7 @@ Hooks.once("socketlib.ready", () => {
             await magical.obj.document.setFlag("world", "detectMagic", true);
             new Sequence()
                 .effect("jb2a.magic_signs.rune.{{school}}.intro.{{color}}")
+                .forUsers(users)
                 .atLocation(magical.obj)
                 .scale(0.25)
                 .delay(magical.delay)
@@ -100,6 +133,7 @@ Hooks.once("socketlib.ready", () => {
                 .waitUntilFinished(-800)
                 .zIndex(0)
                 .effect("jb2a.magic_signs.rune.{{school}}.loop.{{color}}")
+                .forUsers(users)
                 .atLocation(magical.obj)
                 .scale(0.25)
                 .setMustache(magical)
@@ -107,6 +141,7 @@ Hooks.once("socketlib.ready", () => {
                 .waitUntilFinished(-800)
                 .zIndex(1)
                 .effect("jb2a.magic_signs.rune.{{school}}.outro.{{color}}")
+                .forUsers(users)
                 .atLocation(magical.obj)
                 .scale(0.25)
                 .setMustache(magical)
@@ -160,10 +195,10 @@ Hooks.once("socketlib.ready", () => {
         }
 
 
-        async function registerDetectMagicHook(tokenD) {
+        async function registerDetectMagicHook(tokenD, users) {
             let detectMagicHookId = Hooks.on("updateToken", async (tokenDocument, updateData, options) => {
                 //console.log("hook fired!");
-                if (!updateData.x && !updateData.y) return;
+                if ((!updateData.x && !updateData.y) || (tokenDocument.id != tokenD.id)) return;
                 //console.log("Controlled token: " , tokenD);
                 let magicalObjects = [];
                 if (game.modules.get("tagger")?.active) {
@@ -199,6 +234,7 @@ Hooks.once("socketlib.ready", () => {
                     //console.log("Dispalying rune on magical object: ", magical);
                     new Sequence()
                         .effect("jb2a.magic_signs.rune.{{school}}.intro.{{color}}")
+                        .forUsers(users)
                         .atLocation(magical.obj)
                         .scale(0.25)
                         .delay(magical.delay)
@@ -206,6 +242,7 @@ Hooks.once("socketlib.ready", () => {
                         .waitUntilFinished(-800)
                         .zIndex(0)
                         .effect("jb2a.magic_signs.rune.{{school}}.loop.{{color}}")
+                        .forUsers(users)
                         .atLocation(magical.obj)
                         .scale(0.25)
                         .setMustache(magical)
@@ -213,6 +250,7 @@ Hooks.once("socketlib.ready", () => {
                         .waitUntilFinished(-800)
                         .zIndex(1)
                         .effect("jb2a.magic_signs.rune.{{school}}.outro.{{color}}")
+                        .forUsers(users)
                         .atLocation(magical.obj)
                         .scale(0.25)
                         .setMustache(magical)
@@ -227,6 +265,9 @@ Hooks.once("socketlib.ready", () => {
             await tokenD.document.setFlag("world", "detectMagicHookId", detectMagicHookId);
         }
     }
-	socket = socketlib.registerModule("advancedspelleffects");
-	socket.register("detectMagic", detectMagicContinous);
+
+    //register module with socketlib
+    socket = socketlib.registerModule("advancedspelleffects");
+    //register all effect functions defined above with socketlib here
+    socket.register("detectMagic", detectMagicContinous);
 });
