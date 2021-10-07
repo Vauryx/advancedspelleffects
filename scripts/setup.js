5ccf670b6ae782cb09642ea9d0f4c6bb4248cc0d
Faizan Ahmad <cfaizana@gmail.com>
2021-10-07 00:39:18 -0400

    V-0.1
    Big 0.1 update

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,11 +1,19 @@
 import ASESettings from "./aseSettings.js";
-Hooks.on(`renderItemSheet5e`, async (app, html, data) => {
-    console.log("ASE: Caught actor sheet render hook!");
+Hooks.once('ready', async function () {
+  if(!game.user.isGM) return;
+  Hooks.on(`renderItemSheet5e`, async (app, html, data) => {
+    //console.log("ASE: Caught actor sheet render hook!", data);
+    let aseSpellList = ['Darkness', 'Detect Magic', 'Fog Cloud', 'Steel Wind Strike', 'Thunder Step', 'Spiritual Weapon', 'Call Lightning'];
+    if (!aseSpellList.includes(data.item.name)) {
+      return;
+    }
     const aseBtn = $(`<a class="ase-item-settings" title="ASE"><i class="fas fa-biohazard"></i>ASE</a>`);
     aseBtn.click(ev => {
       new ASESettings(app.entity, {}).render(true);
     });
-      html.closest('.app').find('.ase-item-settings').remove();
-      let titleElement = html.closest('.app').find('.window-title');
-      aseBtn.insertAfter(titleElement);
+    html.closest('.app').find('.ase-item-settings').remove();
+    let titleElement = html.closest('.app').find('.window-title');
+    aseBtn.insertAfter(titleElement);
   });
+});
+  
