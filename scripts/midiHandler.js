6a22ef787cc8d09e26ae4d6902b43d5fd9903a48
Faizan Ahmad <cfaizana@gmail.com>
2021-10-23 15:18:01 -0400

    Refactor push 2

--- scripts/midiHandler.js
+++ scripts/midiHandler.js
@@ -2,10 +2,11 @@ import { ASEHandler } from "./ASEHandler.js"
 export class midiHandler {
     static registerHooks() {
         if (game.modules.get("midi-qol")?.active) {
-            Hooks.on("midi-qol.RollComplete", (workflow) => {
-                console.log("MIDI Workflow: ", workflow);
-                ASEHandler.handleASE(workflow)
-            });
+            Hooks.on("midi-qol.RollComplete", midiHandler._handleASE);
         }
+    }
+    static async _handleASE(workflow) {
+        console.log("MIDI Workflow: ", workflow);
+        ASEHandler.handleASE(workflow)
     }
 }
--- scripts/noMidiHandler.js
+++ scripts/noMidiHandler.js
@@ -4,32 +4,34 @@ import { concentrationHandler } from "./concentrationHandler.js";
 export class noMidiHandler {
     static registerHooks() {
         if (!game.modules.get("midi-qol")?.active) {
-            Hooks.on("preCreateChatMessage", async (msg) => {
-                //console.log("Chat Message Data: ", msg);
-                let caster = canvas.tokens.get(msg.data.speaker.token);
-                let casterActor = caster?.actor;
-                let spellItem = casterActor?.items?.getName(msg.data.flavor);
-                let aseSpell = spellItem?.data?.flags?.advancedspelleffects ?? false;
-                if (!caster || !casterActor || !spellItem || !aseSpell) return;
-                let chatContent = msg.data.content;
-                let spellLevel = chatContent.charAt(chatContent.indexOf("data-spell-level")+18);
-                let spellTargets = Array.from(game.user.targets);
-                let data = {
-                    actor: casterActor,
-                    token: caster,
-                    tokenId: msg.data.speaker.token,
-                    item: spellItem,
-                    itemLevel: spellLevel,
-                    targets: spellTargets
-                };
-                if(spellItem.data.data.components.concentration){
-                    await concentrationHandler.addConcentration(casterActor, spellItem);
-                }
-                
-                console.log(data);
-                ASEHandler.handleASE(data);
-                //specialCaseAnimations(msg);
-            });
+            Hooks.on("preCreateChatMessage", noMidiHandler._handleASE);
         }
+    }
+
+    static async _handleASE(msg) {
+        //console.log("Chat Message Data: ", msg);
+        let caster = canvas.tokens.get(msg.data.speaker.token);
+        let casterActor = caster?.actor;
+        let spellItem = casterActor?.items?.getName(msg.data.flavor);
+        let aseSpell = spellItem?.data?.flags?.advancedspelleffects ?? false;
+        if (!caster || !casterActor || !spellItem || !aseSpell) return;
+        let chatContent = msg.data.content;
+        let spellLevel = chatContent.charAt(chatContent.indexOf("data-spell-level")+18);
+        let spellTargets = Array.from(game.user.targets);
+        let data = {
+            actor: casterActor,
+            token: caster,
+            tokenId: msg.data.speaker.token,
+            item: spellItem,
+            itemLevel: spellLevel,
+            targets: spellTargets
+        };
+        if(spellItem.data.data.components.concentration){
+            await concentrationHandler.addConcentration(casterActor, spellItem);
+        }
+        
+        console.log("NO-MIDI DATA: ", data);
+        ASEHandler.handleASE(data);
+        //specialCaseAnimations(msg);
     }
 }
